726cb5cf80d1d7d64ca2591973cf541d08e66a2e
cscmobility <mobilab@Mobilitys-iMac.local>
2014-07-10 14:18:59 +0700

    initial

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  helloios
+//
+//  Created by Mobility Lab on 7/9/14.
+//  Copyright (c) 2014 Mobility Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  helloios
+//
+//  Created by Mobility Lab on 7/9/14.
+//  Copyright (c) 2014 Mobility Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
